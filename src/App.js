1deab3d912f77fff7d91dc669ce168891e13c472
Abderrahmane <abdrr97@gmail.com>
2021-05-29 10:31:40 +0100

    adding css transitions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Counter } from './components/functions/counter'
+import DarkMode from './components/functions/dark-mode'
+// import { Counter } from './components/functions/counter'
+
 function App() {
   return (
-    <div className='container my-5 '>
+    <main className='container my-5 '>
       <h1 className='display-1'>Hello React App</h1>
-      <Counter />
-    </div>
+      <DarkMode />
+    </main>
   )
 }
 
--- src/components/functions/dark-mode.jsx
+++ src/components/functions/dark-mode.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const DarkMode = () => {
-  return <div></div>
+  const [isDark, setIsDark] = useState(false)
+
+  document.body.className = isDark ? 'dark-mode' : ''
+  document.title = isDark ? '⚛ React Dark Mode' : '⚛ React Light Mode'
+  const toggleDarkMode = () => {
+    setIsDark(!isDark)
+  }
+  return (
+    <>
+      <button onClick={toggleDarkMode} className='dark-button'></button>
+      <h3>Blog Dark Mode</h3>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui porro
+        deserunt a optio quaerat. Fugiat esse iusto unde ducimus modi obcaecati
+        nesciunt delectus velit laudantium iste minus vel voluptate, tenetur
+        facere natus illum architecto, sapiente amet accusantium maxime
+        asperiores. Excepturi.
+      </p>
+    </>
+  )
 }
 
 export default DarkMode

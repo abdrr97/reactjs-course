ea2a32e4136f86de7a10637a419d81d03056d3ce
Abderrahmane <abdrr97@gmail.com>
2021-06-01 12:34:23 +0100

    fixing and changing some things

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import { users } from './users'
 const App = () => {
   const [userList, setUserList] = useState(users)
 
+  const deleteUser = (id) => {
+    const newUserList = userList.filter((user) => user.id !== id)
+    setUserList(newUserList)
+  }
+
   return (
     <main className='container my-5'>
       <h3 className='fw-light'>Users List</h3>
@@ -12,9 +17,23 @@ const App = () => {
         {userList.map((user) => {
           const { id, username, email, avatar } = user
           return (
-            <li key={id} className='list-group-item'>
-              <img width='50' src={avatar} alt={username} />
-              {username} <small>{email}</small>
+            <li
+              key={id}
+              className='d-flex justify-content-between align-items-center list-group-item'
+            >
+              <div>
+                <img width='50' src={avatar} alt={username} />
+                {username}
+                <strong>
+                  <u>{email}</u>
+                </strong>
+              </div>
+              <button
+                onClick={() => deleteUser(id)}
+                className='btn btn-sm btn-danger'
+              >
+                &times;
+              </button>
             </li>
           )
         })}
@@ -23,7 +42,7 @@ const App = () => {
         Clear All
       </button>
       <button
-        onClick={() => setUserList([])}
+        onClick={() => setUserList(users)}
         className='mx-2 btn btn-primary my-3'
       >
         Refresh
